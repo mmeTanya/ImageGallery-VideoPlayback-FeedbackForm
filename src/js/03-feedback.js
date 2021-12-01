d99a81cfb18f2474051721f191951f4dfd41cf23
Tetiana Legois <mme.tanyalegois@gmail.com>
2021-12-01 19:02:17 +0200

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -11,7 +11,10 @@ const formData = {};
 controlAutoFillingForm();
 
 function onTextareaInput(e) {
-  formData[e.target.name] = e.target.value;
+  const formData = {
+    email: email.value,
+    message: message.value,
+  };
   localStorage.setItem('feedback - form - state', JSON.stringify(formData));
   return formData;
 }
